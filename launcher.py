2f1f22a02edc1e6f3d11f617179e3da111248e08
Panagiotis3149 <138581609+Panagiotis3149@users.noreply.github.com>
2024-06-12 10:39:57 +0000

    Reworked + FIxed launcher.py

--- launcher.py
+++ launcher.py
@@ -1,121 +1,112 @@
-import tkinter as tk
 import os
-from tkinter import messagebox
 import random
+import webbrowser
+import customtkinter as customtkinter  # type: ignore
 
-def generateRandomUser():
-    num = "123456789069696969"
+# Set CustomTkinter appearance mode and color theme
+customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("green")  # Themes: blue (default), dark-blue, green
+
+
+def generate_random_user():
+    num = "1234567890"
     char = "qwertyuiopasdfghjklzxcvbnm"
 
     random_string = ""
 
     for i in range(10):
-        random_string = random_string +  random.choice(char)
+        random_string += random.choice(char)
     for i in range(4):
-        random_string = random_string + random.choice(num)
+        random_string += random.choice(num)
 
     usernameValue.set(random_string)
-
-def setJavaPath():
-    javaPath.set("javaw")
+# Function to display initial warning about RATs
 
 
+def display_initial_warning():
+    return customtkinter.CTkMessagebox.askyesno("Warning", "Running certain applications may expose your system to Remote Access Trojans (RATs). Exercise caution and consider installing HostsMod for additional protection.")
 
-def runAstolfo():
+# Function to display second warning before running potentially risky application
+def display_second_warning():
+    return customtkinter.CTkMessagebox.askyesno("Warning", "You are about to run an application that may be risky. Do you want to proceed?")
 
-    root.withdraw()
-    file = open("temprunner.bat", "w")
-    file.write("@echo off" + "\n")
-    file.write("set folder=%appdata%" + "\n")
-    
+# Function to prompt user to install HostsMod
+def prompt_install_hostsmod():
+    response = customtkinter.CTkMessagebox.askyesno("Install HostsMod", "HostsMod is not installed on your system. Do you want to install it for additional protection?")
+    if response:
+        # Open the browser to download HostsMod
+        webbrowser.open("https://github.com/GardeningTool/HostsMod/releases/tag/2.0.1/HostsMod.exe")
 
-    
-    bigstring = f'{javaPath.get()} -noverify -Xms2024M -Xmx4048M -Djava.library.path="natives" -cp "libs";"release.jar" net.minecraft.client.main.Main --accessToken vcyegwgdhugegwhy --ver Astolfo --gameDir "%folder%\\.minecraft" --assetsDir "assets" --assetIndex 1.8 --username {usernameValue.get()} --launchToken a'
-    file.write(bigstring + "\n")
-    file.close()
-    os.system("temprunner.bat")
-    os.remove("temprunner.bat")
+# Function to run Astolfo application
+def run_astolfo():
+    if display_initial_warning():
+        if display_second_warning():
+            prompt_install_hostsmod()
+            # Proceed with running Astolfo application
+            with open("temprunner.bat", "w") as file:
+                file.write("@echo off\n")
+                file.write("set folder=%appdata%\n")
 
-    root.deiconify()
+                bigstring = f'{javaPath.get()} -noverify -Xms2024M -Xmx4048M -Djava.library.path="natives" -cp "libs";"release.jar" net.minecraft.client.main.Main --accessToken vcyegwgdhugegwhy --ver Astolfo --gameDir "%folder%\\.minecraft" --assetsDir "assets" --assetIndex 1.8 --username {usernameValue.get()} --launchToken a' # type: ignore
+                file.write(bigstring + "\n")
+            
+            os.system("temprunner.bat")
+            os.remove("temprunner.bat")
 
+# Function to display credits and information
+def show_info():
+    root1 = customtkinter.CTk()
+    root1.title("Information")
+    root1.geometry("250x250")  # Set the window size to 200x200
+    root1.resizable(True, True)
 
-def showInfo():
-
-    
-    root1 = tk.Tk()
-    root1.title("credits")
-    root1.resizable(False, False)
     info = """
-Astolfo Launcher
+Unofficial Launcher For CRACKED Astolfo
 Made by enorsu
-Python 3.12.3
-Alpha Build 1.6.1
+Reworked by Panagiotis3149
+Python 3.12.4/3.12.3
+Alpha Build 1.7.3
 
 Astolfo Client Crack by AllahLeaks
-        """
-    lbl5 = tk.Label(root1, text=info, font=("Helvetica", 10))
-    lbl5.pack()
+    """
 
-    closeInfo = tk.Button(root1, text="close", padx=15, pady=3, cursor="hand2", command=root1.destroy)
-    closeInfo.pack()
+    info_box = customtkinter.CTkTextbox(root1, width=250, height=250)  # Create a textbox with 250x250 size
+    info_box.insert("0.0", info)
+    info_box.configure(state="disabled")  # Make the textbox read-only
+    info_box.pack(pady=10, padx=10)
+
+    close_info_btn = customtkinter.CTkButton(root1, text="Close", width=120, height=32, cursor="hand2", command=root1.destroy)
+    close_info_btn.pack(pady=10, padx=10)
+    
     root1.mainloop()
 
+def label_clicked():
+    show_info()
 
-
-def label_clicked(event):
-    #root.withdraw()
-    #messagebox.showinfo("credits", info)
-    #root.deiconify()
-
-    showInfo()
-
-
-
-root = tk.Tk()
+# Create the root window
+root = customtkinter.CTk()
 root.title("Astolfo Launcher")
-root.geometry("300x260")
-root.resizable(False,False)
+root.geometry("390x250")
+root.resizable(False, False)
 
+usernameValue = customtkinter.StringVar(value="AstolfoRATTED")
 
-usernameValue = tk.StringVar()
-usernameValue.set("astolfo")
+# Username Label and Entry
+username_label = customtkinter.CTkLabel(root, text="Username:")
+username_label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
 
-lbl1 = tk.Label(root, text="Astolfo Launcher", font=("Helvetica", 16))
-lbl1.pack(anchor="center")
+usernameInput = customtkinter.CTkEntry(root, textvariable=usernameValue)
+usernameInput.grid(row=0, column=1, padx=20, pady=10)
 
-javaPath = tk.StringVar()
-javaPath.set("javaw")
+# Launch Astolfo Button
+launch_button = customtkinter.CTkButton(root, text="Launch Astolfo", cursor="hand2", command=run_astolfo)
+launch_button.grid(row=3, column=1, padx=20, pady=10)
 
+launch_button = customtkinter.CTkButton(root, text="Randomize Username", cursor="hand2", command=generate_random_user)
+launch_button.grid(row=1, column=1, padx=20, pady=10)
 
-lbl2 = tk.Label(root, text="Username:")
-lbl2.pack(anchor="center")
-
-usernameInput = tk.Entry(root, textvariable=usernameValue)
-usernameInput.pack(anchor="center")
-
-#doRandomize = tk.IntVar()
-
-#randomizeUserCheckbox = tk.Checkbutton(root, text="Randomize username", variable=doRandomize)
-#randomizeUserCheckbox.pack(anchor="center")
-
-runBtn = tk.Button(root, text="Run", padx=15, pady=3, cursor="hand2", command=runAstolfo)
-runBtn.pack(anchor="center", pady=10)
-
-randomizeBtn = tk.Button(root, text="Randomize username", padx=15, pady=3, cursor="hand2", command=generateRandomUser)
-randomizeBtn.pack(anchor="center")
-
-
-lbl3 = tk.Label(root, text="click for more info",font=("Helvetica", 9), cursor="hand2")
-lbl3.pack(anchor="center", side="bottom")
-lbl3.bind("<Button-1>", label_clicked)
-
-javaInput = tk.Entry(root, textvariable=javaPath)
-javaInput.pack(anchor="center", side="bottom", pady=10)
-
-lbl4 = tk.Label(root, text="Java path:",font=("Helvetica", 9))
-lbl4.pack(anchor="center", side="bottom")
-
-
+# Info Button
+info_button = customtkinter.CTkButton(root, text="Info", cursor="hand2", command=label_clicked, fg_color="#379634")
+info_button.grid(row=4, column=1, padx=20, pady=10)
 
 root.mainloop()
-
